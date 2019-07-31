0053aab2d5945af74a14fcad6a29453c4102d05f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-07-31 17:25:10 +0800

    replace require.Nil(t, err, err) -> require.NoError(t, err)

--- routing_windows_test.go
+++ routing_windows_test.go
@@ -8,24 +8,24 @@ import (
 )
 
 func TestRoute(t *testing.T) {
-	iface, err := selectInterfaces("Ethernet0")
-	require.Nil(t, err)
+	iface, err := selectInterface("Ethernet0")
+	require.NoError(t, err)
 	route, err := newRouter(iface)
-	require.Nil(t, err)
+	require.NoError(t, err)
 	// ipv4
 	g, src, err := route.route(net.ParseIP("192.168.1.1"))
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.Nil(t, g)
 	t.Log("src:", src)
 	g, src, err = route.route(net.ParseIP("1.1.1.1"))
-	require.Nil(t, err)
+	require.NoError(t, err)
 	t.Log("gateway:", g, "src:", src)
 	// ipv6
 	g, src, err = route.route(net.ParseIP("fe80::1"))
-	require.Nil(t, err)
+	require.NoError(t, err)
 	require.Nil(t, g)
 	t.Log("src:", src)
 	g, src, err = route.route(net.ParseIP("240c::1"))
-	require.Nil(t, err)
+	require.NoError(t, err)
 	t.Log("gateway:", g, "src:", src)
 }
