9d6346030d3540ee968c49a7a640e61833de00c8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-08-01 12:03:44 +0800

    add opt: Method Device Rate

--- options.go
+++ options.go
@@ -1,23 +1,31 @@
 package scanner
 
 import (
-	"runtime"
 	"strings"
 	"time"
 )
 
+const (
+	MethodSYN     = "syn"
+	MethodConnect = "connect"
+)
+
 type Options struct {
-	LocalIP    string
-	Goroutines int
-	Timeout    time.Duration
+	Method  string // "syn", "connect"
+	Device  string
+	Rate    int
+	Timeout time.Duration
 }
 
 func (opt *Options) apply() {
-	if opt.Goroutines < 1 {
-		opt.Goroutines = 512 * runtime.NumCPU()
+	if opt.Method == "" {
+		opt.Method = MethodSYN
+	}
+	if opt.Rate < 1 {
+		opt.Rate = 1000
 	}
 	if opt.Timeout < 1 {
-		opt.Timeout = 3 * time.Second
+		opt.Timeout = 10 * time.Second
 	}
 }
 
