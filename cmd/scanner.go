7d675cdc76807a4cc85c48db16c6b4b6b2a09f19
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-08-06 20:16:08 +0800

    update cmd: add options

--- cmd/scanner.go
+++ cmd/scanner.go
@@ -20,27 +20,30 @@ func main() {
 		rate    int
 		timeout time.Duration
 		workers int
+		senders int
 		save    string
 	)
 	targetsUsage := bytes.Buffer{}
-	targetsUsage.WriteString("192.168.1.1, fe80::1, ")
-	targetsUsage.WriteString("192.168.1.1-192.168.1.3, fe80::1-fe80::1, ")
+	targetsUsage.WriteString("192.168.1.1, fe80::1\n")
+	targetsUsage.WriteString("192.168.1.1-192.168.1.3, fe80::1-fe80::1\n")
 	targetsUsage.WriteString("192.168.1.1/24")
-	flag.StringVar(&targets, "targets", "127.0.0.1", targetsUsage.String())
-	flag.StringVar(&ports, "ports", "80", "80, 80-82")
+	flag.StringVar(&targets, "targets", "", targetsUsage.String())
+	flag.StringVar(&ports, "ports", "", "80, 81-82")
 	flag.StringVar(&method, "method", scanner.MethodSYN, "connect or syn")
 	flag.StringVar(&device, "device", "", "interface name Ethernet0, eth0")
-	flag.IntVar(&rate, "rate", 1000, "packet send per second")
-	flag.DurationVar(&timeout, "timeout", 0, "timeout")
-	flag.IntVar(&workers, "workers", 0, "scanner number. usually don't need set")
+	flag.IntVar(&rate, "rate", 1000, "send packet rate")
+	flag.DurationVar(&timeout, "timeout", 5*time.Second, "timeout")
+	flag.IntVar(&workers, "workers", 0, "scanner number")
+	flag.IntVar(&senders, "senders", 0, "packet sender number")
 	flag.StringVar(&save, "save", "", "save scan result")
 	flag.Parse()
-	opts := &scanner.Options{
+	opts := scanner.Options{
 		Method:  method,
 		Device:  device,
 		Rate:    rate,
 		Timeout: timeout,
 		Workers: workers,
+		Senders: senders,
 	}
 	if save != "" {
 		file, err := os.OpenFile(save, os.O_CREATE|os.O_APPEND, 644)
@@ -51,7 +54,7 @@ func main() {
 		log.SetOutput(&logger{file: file})
 	}
 	start := time.Now()
-	s, err := scanner.New(targets, ports, opts)
+	s, err := scanner.New(targets, ports, &opts)
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -64,18 +67,14 @@ func main() {
 		signal.Notify(signalChan, os.Kill, os.Interrupt)
 		<-signalChan
 		s.Stop()
-		log.Print("stop scanner\r\n")
+		log.Print("stop scanner.\r\n")
 	}()
-	result := make(map[string]struct{})
+	var scanned int
 	for address := range s.Result {
-		_, ok := result[address]
-		if ok {
-			log.Printf("duplicate: %s\r\n", address)
-			continue
-		}
-		result[address] = struct{}{}
+		scanned += 1
+		log.Print(address + "\r\n")
 	}
-	log.Printf("scan finished. total: %d time: %s\r\n", len(result), time.Since(start))
+	log.Printf("scan finished. total: %d time: %s\r\n", scanned, time.Since(start))
 }
 
 type logger struct {
