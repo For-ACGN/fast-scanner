23668f099e2f2f33569c4badd357fa89cf36a63d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2019-08-06 14:00:11 +0800

    add options: Senders and Raw and rename test name

--- options.go
+++ options.go
@@ -12,11 +12,20 @@ const (
 )
 
 type Options struct {
-	Method  string // "syn", "connect"
+	// "syn", "connect"
+	Method  string
 	Device  string
 	Rate    int
 	Timeout time.Duration
+	// "connect": connectScanner() goroutine number
+	// "syn": synScanner() and synParser() goroutine number
 	Workers int
+	// packetSender number useless for "connect"
+	Senders int
+	// if true scanner will not handle duplicate result,
+	// it will use less memory
+	// default will handle duplicate result
+	Raw bool
 }
 
 func (opt *Options) apply() {
@@ -27,14 +36,29 @@ func (opt *Options) apply() {
 		opt.Rate = 1000
 	}
 	if opt.Timeout < 1 {
-		opt.Timeout = 5 * time.Second
+		switch opt.Method {
+		case MethodConnect:
+			opt.Timeout = 5 * time.Second
+		case MethodSYN:
+			opt.Timeout = 2 * time.Second
+		}
 	}
 	if opt.Workers < 1 {
 		switch opt.Method {
 		case MethodConnect:
-			opt.Workers = 128 * runtime.NumCPU()
+			opt.Workers = 64 * runtime.NumCPU()
 		case MethodSYN:
-			opt.Workers = 1
+			opt.Workers = 32 * runtime.NumCPU()
+		}
+	}
+	if opt.Senders < 1 {
+		switch runtime.GOOS {
+		case "windows":
+			opt.Senders = 2 // "magic send packet speed"
+		case "linux":
+			opt.Senders = 1 // "one goroutine will get full speed"
+		default:
+			opt.Senders = 1
 		}
 	}
 }
